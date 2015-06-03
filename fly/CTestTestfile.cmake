d317f4a3c4ec0c98fb9c02690bf32eb60b23db58
Esteban Rivera <negro6c27@gmail.com>
2015-06-03 17:49:41 -0500

    Takeoff

---
+++ fly/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/edrone/catkin_ws/src/fly
+# Build directory: /home/edrone/catkin_ws/src/fly
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
