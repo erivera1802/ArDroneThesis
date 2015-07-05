0050bccb19bdaa861438bb5d974b951fb0d813a6
Esteban Rivera <negro6c27@gmail.com>
2015-07-05 12:31:59 -0500

    new node receiving from tracking and sending to drone

---
+++ control/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "control")
+set(control_MAINTAINER "edrone <edrone@todo.todo>")
+set(control_DEPRECATED "")
+set(control_VERSION "0.0.0")
+set(control_BUILD_DEPENDS "ardrone_autonomy" "geometry_msgs" "image_transport" "roscpp" "sensor_msgs" "std_msgs" "tf")
+set(control_RUN_DEPENDS "ardrone_autonomy" "geometry_msgs" "image_transport" "roscpp" "sensor_msgs" "std_msgs" "tf")
+set(control_BUILDTOOL_DEPENDS "catkin")
